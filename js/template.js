b99b598518bbe3a595d26889b649da646996f13a
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2019-01-26 16:49:10 +0100

    Rewrite with templating

---
+++ js/template.js
@@ -0,0 +1,9 @@
+$(document).ready(function (e) {
+    $('head').prepend('/en/templates/head.html');
+    $('#facebook').load('/en/templates/facebook.html');
+    $('#flags').load('/en/templates/flags.html');
+    $('#logo').load('/en/templates/logo.html');
+    $('footer').load('/en/templates/footer.html');
+    $('#scripts').load('/en/templates/scripts.html');
+    $('#lava').load('/en/templates/lava.html');
+});
